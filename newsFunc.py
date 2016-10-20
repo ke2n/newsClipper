3150edbef29f7b85b50a9e2029dbd2da0af18bb1
EC2 Default User <ec2-user@ip-172-31-5-220.ap-northeast-2.compute.internal>
2016-10-20 00:57:29 +0000

    function 추가 및 수정

--- newsFunc.py
+++ newsFunc.py
@@ -1,6 +1,8 @@
 from goose import Goose
 from goose.text import StopWordsKorean
+import newspaper
 import urlparse
+
 
 def getArticles(url):
     parsed = urlparse.urlparse(url)
@@ -15,12 +17,31 @@ def getArticles(url):
 
     try:
         image_src = article.top_image.src
+        
     except AttributeError as e:
         image_src = None
 
+    text_val = article.cleaned_text.replace("\n\n", "\n")
+
+    if len(text_val) > 10:
+        returnVal = {
+            "link": url,
+            "text": text_val,
+            "image": image_src
+        }
+    else:
+        returnVal = getArticles_new(url)
+
+    return returnVal
+
+def getArticles_new(url):
+    article = newspaper.Article(url)
+    article.download()
+    article.parse()
+    
     returnVal = {
         "link": url,
-        "text": article.cleaned_text.replace("\n\n", "\n"),
-        "image": image_src
+        "text": article.text.replace("\n\n", "\n"),
+        "image": article.top_image
     }
     return returnVal
